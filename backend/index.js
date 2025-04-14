9433935fad3ce187d9391279e305c3ca68e347ea
Ronaldo-001 <ronaldop1705@gmail.com>
2025-04-14 07:52:41 +0000

    finished quote app - v01

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,25 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.json()); // Parses JSON in requests
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+const quotes = [
+  "Push yourself, because no one else is going to do it for you.",
+  "Success doesn't just find you. You have to go out and get it.",
+  "The harder you work for something, the greater you'll feel when you achieve it.",
+  "Dream it. Wish it. Do it.",
+  "Great things never come from comfort zones.",
+  "Don’t stop when you’re tired. Stop when you’re done.",
+  "Wake up with determination. Go to bed with satisfaction.",
+  "Do something today that your future self will thank you for.",
+  "It’s going to be hard, but hard does not mean impossible.",
+  "Sometimes later becomes never. Do it now."
+];
+
+app.get('/api/quote', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  res.json({ quote: quotes[randomIndex] });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Backend running at http://localhost:${PORT}`);
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,12 @@
- fetch("http://localhost:3000/api/hello") // replace with EC2 IP later
-  .then((res) => res.json())
-  .then((data) => {
-    document.getElementById("msg").textContent = data.message;
-  })
-  .catch((err) => {
-    console.error("Error:", err);
-    document.getElementById("msg").textContent = "Failed to load backend message.";
+document.getElementById("getQuoteBtn").addEventListener("click", () => {
+    fetch("http://localhost:3000/api/quote") // replace with EC2 IP later
+      .then((res) => res.json())
+      .then((data) => {
+        document.getElementById("msg").textContent = `"${data.quote}"`;
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        document.getElementById("msg").textContent = "Failed to load quote.";
+      });
   });
+  
